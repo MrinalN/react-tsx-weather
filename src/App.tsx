13b26386bf5f17fc2c805bf1559c542da66a7b89
Adele Noronha <adele.c.noronha@gmail.com>
2021-07-01 15:49:24 +0000

    pass props to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React, {useState} from 'react';
+import { LocationSearch } from './LocationSearch';
 import './App.css';
 
 function App() {
-  const [locationSearch, setLocationSearch] = useState('Paris');
+  // const [locationSearch, setLocationSearch] = useState('Paris');
   const [locations, setLocations] = useState<string[]>([]); //an empty array cannot be inferred so specifying generic parameter
-  const disableSearch = locationSearch.trim() === '';
-  const addLocation = () => {
-    setLocations([locationSearch, ...locations]); 
-    setLocationSearch('');
-  }
+  const addLocation = (location: string) => setLocations([location, ...locations])
+  // const disableSearch = locationSearch.trim() === '';
+  // const addLocation = () => {
+  //   setLocations([locationSearch, ...locations]); 
+  //   setLocationSearch('');
+  // }
   return (
     <div className="container">
       <h1>Weather App</h1>
-      <div>
-        <label>
-          Add Location <input className="ml-1 mr-1" type="text" value={locationSearch} onChange={e => setLocationSearch(e.target.value)}/>
-        </label>
-        <button className="btn btn-primary" onClick={addLocation} disabled={disableSearch}>Search</button>
-      </div>
+      
+      <LocationSearch onSearch={addLocation}/>
 
       <div>
         <h2>Locations</h2>
