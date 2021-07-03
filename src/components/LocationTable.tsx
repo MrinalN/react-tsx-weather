be2cae4ba130d068e85f065449911ac91f8088ae
Adele Noronha <adele.c.noronha@gmail.com>
2021-07-03 19:26:03 +0000

    destructure them in component. wire click handler

--- src/components/LocationTable.tsx
+++ src/components/LocationTable.tsx
@@ -3,9 +3,11 @@ import { WeatherLocation } from "../model/Weather";
 
 interface LocationTableProps {
   locations: WeatherLocation[];
+  current: WeatherLocation | null;
+  onSelect: (location: WeatherLocation) => void;
 }
 
-export const LocationTable: FC<LocationTableProps> = ({locations}) =>
+export const LocationTable: FC<LocationTableProps> = ({locations, current, onSelect}) =>
   <div>
     <h2>Locations</h2>
     <table className="table table-hover">
@@ -16,7 +18,11 @@ export const LocationTable: FC<LocationTableProps> = ({locations}) =>
       </thead>
       <tbody>
       {locations.map((location) =>
-        <tr><td>{location.name}</td></tr>
+        <tr key={location.id}
+            className={current?.id === location.id ? 'table-primary' : ''} 
+            onClick={() => onSelect(location)}>
+          <td>{location.name}</td>
+        </tr>
       )}
       </tbody>
     </table>
